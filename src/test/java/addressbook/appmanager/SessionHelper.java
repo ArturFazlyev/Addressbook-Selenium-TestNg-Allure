c26c157bcb6711f027990d21a281f14e61fcedfa
fazlyev.art@gmail.com <Futurefavourite123>
2019-03-17 18:03:27 +0500

    создание вспомогательного класса

--- src/test/java/addressbook/appmanager/SessionHelper.java
+++ src/test/java/addressbook/appmanager/SessionHelper.java
@@ -3,16 +3,15 @@ package addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class SessionHelper {
-    private FirefoxDriver wd;
+public class SessionHelper extends HelperBase {
 
     public SessionHelper(FirefoxDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void login(String username, String password) {
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
+        type(By.name("user"), username);
+        type(By.name("pass"), password );
+        click(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]"));
     }
 }
