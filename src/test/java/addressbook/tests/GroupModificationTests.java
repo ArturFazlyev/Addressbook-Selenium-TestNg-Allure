484ec8613143910d8f72372025b419ee310db8d7
fazlyev.art@gmail.com <Futurefavourite123>
2019-03-17 19:28:54 +0500

    тест модификации

---
+++ src/test/java/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,20 @@
+package addressbook.tests;
+
+import addressbook.module.GroupData;
+import org.testng.annotations.Test;
+
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void testGroupModification(){
+        app.getNavigationHelper().goToGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+
+
+
+    }
+}
