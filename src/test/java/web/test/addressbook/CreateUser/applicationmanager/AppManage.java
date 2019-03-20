cb714d4864f96252e6b162d8cf777ed49fb17bfa
fazlyev.art@gmail.com <Futurefavourite123>
2019-03-20 20:37:58 +0500

    добавлен класс помощник

--- src/test/java/web/test/addressbook/CreateUser/applicationmanager/AppManage.java
+++ src/test/java/web/test/addressbook/CreateUser/applicationmanager/AppManage.java
@@ -1,20 +1,21 @@
 package web.test.addressbook.CreateUser.applicationmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
-import web.test.addressbook.CreateUser.module.ContactData;
 
 import java.util.concurrent.TimeUnit;
 
 public class AppManage {
-    public WebDriver wd;
+     FirefoxDriver wd;
+
+    private ContactHelper contactHelper = new ContactHelper(wd);
+
 
     public void init() {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
+        contactHelper = new ContactHelper(wd);
         login("admin", "secret");
     }
 
@@ -28,54 +29,6 @@ public class AppManage {
       wd.findElement(By.xpath("//input[@value='Login']")).click();
     }
 
-    public void returnHomePage() {
-      wd.findElement(By.linkText("home page")).click();
-    }
-
-    public void submitNewContact() {
-      wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
-    }
-
-    public void createNewContact(ContactData contactData) {
-      wd.findElement(By.name("firstname")).click();
-      wd.findElement(By.name("firstname")).clear();
-      wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-      wd.findElement(By.name("lastname")).click();
-      wd.findElement(By.name("lastname")).clear();
-      wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-      wd.findElement(By.name("nickname")).click();
-      wd.findElement(By.name("nickname")).clear();
-      wd.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
-      wd.findElement(By.name("title")).click();
-      wd.findElement(By.name("title")).clear();
-      wd.findElement(By.name("title")).sendKeys(contactData.getTitle());
-      wd.findElement(By.name("theform")).click();
-      wd.findElement(By.name("company")).click();
-      wd.findElement(By.name("company")).clear();
-      wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
-      wd.findElement(By.name("address")).click();
-      wd.findElement(By.name("address")).clear();
-      wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-      wd.findElement(By.name("home")).click();
-      wd.findElement(By.name("home")).clear();
-      wd.findElement(By.name("home")).sendKeys(contactData.getHome());
-      wd.findElement(By.name("mobile")).click();
-      wd.findElement(By.name("mobile")).clear();
-      wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
-      wd.findElement(By.name("email")).click();
-      wd.findElement(By.name("email")).clear();
-      wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
-      wd.findElement(By.name("bday")).click();
-      new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getBday());
-      wd.findElement(By.xpath("//option[@value='17']")).click();
-      wd.findElement(By.name("bmonth")).click();
-      new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getBmonth());
-      wd.findElement(By.xpath("//option[@value='January']")).click();
-      wd.findElement(By.name("byear")).click();
-      wd.findElement(By.name("byear")).clear();
-      wd.findElement(By.name("byear")).sendKeys(contactData.getByear());
-    }
-
     public void gotoNewContact() {
       wd.findElement(By.linkText("add new")).click();
     }
@@ -83,5 +36,9 @@ public class AppManage {
     public void close() {
         wd.findElement(By.linkText("Logout")).click();
         wd.quit();
+    }
+
+    public ContactHelper getContactHelper() {
+        return contactHelper;
     }
 }
