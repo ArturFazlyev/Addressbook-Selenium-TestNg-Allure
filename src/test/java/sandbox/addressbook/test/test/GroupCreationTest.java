49697a56adc0f27ddd1088c672be23b24d1c99db
fazlyev.art@gmail.com <Futurefavourite123>
2019-04-14 19:21:49 +0500

    проверки в тесте создания группы

--- src/test/java/sandbox/addressbook/test/test/GroupCreationTest.java
+++ src/test/java/sandbox/addressbook/test/test/GroupCreationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import sandbox.addressbook.test.module.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTest extends TestBase {
@@ -12,10 +13,21 @@ public class GroupCreationTest extends TestBase {
     public void testGroupCreation() throws Exception {
         appManage.getNavigationHelper().goToGroupPage();
         List<GroupData> before = appManage.getGroupHelper().getGroupList();
-        appManage.getGroupHelper().createGroup((new GroupData("test1",
-                "test2", "test3")));
+        GroupData group = new GroupData("test1",
+                "test2", "test3");
+        appManage.getGroupHelper().createGroup(group);
         List<GroupData> after = appManage.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
+
+        int max = 0;
+        for (GroupData g : after){
+            if (g.getId() > max){
+                max = g.getId();
+            }
+        }
+        group.setId(max);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 
 }
