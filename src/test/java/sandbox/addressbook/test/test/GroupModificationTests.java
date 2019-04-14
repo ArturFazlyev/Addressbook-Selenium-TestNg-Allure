f05c86af6d951c5a621688ec54bc4b1b54b775db
fazlyev.art@gmail.com <Futurefavourite123>
2019-04-14 19:03:09 +0500

    сравнение коллекций

--- src/test/java/sandbox/addressbook/test/test/GroupModificationTests.java
+++ src/test/java/sandbox/addressbook/test/test/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import sandbox.addressbook.test.module.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -18,11 +19,17 @@ public class GroupModificationTests extends TestBase {
         List<GroupData> before = appManage.getGroupHelper().getGroupList();
         appManage.getGroupHelper().selectGroup(before.size() - 1);
         appManage.getGroupHelper().initGroupModification();
-        appManage.getGroupHelper().fillGroupFields(new GroupData("test1",
-                "test2", "test3"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(),"test1",
+                "test2", "test3");
+        appManage.getGroupHelper().fillGroupFields(group);
         appManage.getGroupHelper().updateGroup();
         appManage.getGroupHelper().returnToGroupPage();
         List<GroupData> after = appManage.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
     }
 }
