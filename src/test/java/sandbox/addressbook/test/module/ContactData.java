55ccd3d7d31d0ca3506c35f441047bfd6dedc695
fazlyev.art@gmail.com <Futurefavourite123>
2019-04-20 13:39:27 +0500

    проверки контактов

--- src/test/java/sandbox/addressbook/test/module/ContactData.java
+++ src/test/java/sandbox/addressbook/test/module/ContactData.java
@@ -1,5 +1,7 @@
 package sandbox.addressbook.test.module;
 
+import java.util.Objects;
+
 public class ContactData {
     private final String firstname;
     private final String lastname;
@@ -81,8 +83,34 @@ public class ContactData {
         return byear;
     }
 
-
-    public String getGroup() {
+    public String getGroup(){
         return group;
     }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ContactData that = (ContactData) o;
+        return Objects.equals(firstname, that.firstname) &&
+                Objects.equals(lastname, that.lastname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstname, lastname);
+    }
+
+    @Override
+    public String toString() {
+        return "ContactData{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+
+
+
+
 }
