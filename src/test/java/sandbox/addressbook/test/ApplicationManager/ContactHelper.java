6cad2449d6f38ffa7541f63f338db7c4faa7dfa2
fazlyev.art@gmail.com <Futurefavourite123>
2019-04-20 13:41:58 +0500

    проверки контактов

--- src/test/java/sandbox/addressbook/test/ApplicationManager/ContactHelper.java
+++ src/test/java/sandbox/addressbook/test/ApplicationManager/ContactHelper.java
@@ -1,11 +1,8 @@
 package sandbox.addressbook.test.ApplicationManager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
-import org.testng.Assert;
 import sandbox.addressbook.test.module.ContactData;
 
 import java.util.ArrayList;
