e58c17f13470bdc5dfaab11e523c2679214b89fd
artur <fazlyev.art@gmail.com>
2019-05-30 23:13:10 +0500

    проверки

--- src/test/java/sandbox/addressbook/test/test/GroupDeletionTest.java
+++ src/test/java/sandbox/addressbook/test/test/GroupDeletionTest.java
@@ -1,10 +1,16 @@
 package sandbox.addressbook.test.test;
 
+import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import sandbox.addressbook.test.modele.GroupData;
+import sandbox.addressbook.test.modele.Groups;
 
 import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.*;
+import static org.testng.Assert.*;
 
 public class GroupDeletionTest extends TestBase {
 
@@ -18,12 +24,12 @@ public class GroupDeletionTest extends TestBase {
 
     @Test
     public void testGroupDeletion() {
-        Set<GroupData> before = app.group().all();
+        Groups before = app.group().all();
         GroupData deletedGroup = before.iterator().next();
         app.group().delete(deletedGroup);
-        Set<GroupData> after = app.group().all();
-        before.remove(deletedGroup);
-        Assert.assertEquals(before, after);
+        Groups after = app.group().all();
+        assertEquals(after.size(), before.size() - 1);
+        assertThat(after, equalTo(before.without(deletedGroup)));
     }
 }
 
