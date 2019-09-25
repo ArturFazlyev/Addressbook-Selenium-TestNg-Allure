7a2a93e7f7680a13baf3749f0c7681ede71c209a
artur <fazlyev.art@gmail.com>
2019-09-25 12:59:53 +0500

    реализована загрузка данных из файла для контактов

--- src/test/java/sandbox/addressbook/test/test/ContactCreationTest.java
+++ src/test/java/sandbox/addressbook/test/test/ContactCreationTest.java
@@ -1,10 +1,14 @@
 package sandbox.addressbook.test.test;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import sandbox.addressbook.test.modele.ContactData;
 import sandbox.addressbook.test.modele.Contacts;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -12,22 +16,31 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTest extends TestBase {
 
-    @Test
-    public void initContactCreation() {
+    @DataProvider
+    public Iterator<Object[]> validContacts() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+        BufferedReader reader = new BufferedReader(new FileReader(
+                new File("src/test/java/sandbox/addressbook/test/resourses/contacts.json")));
+        String line = reader.readLine();
+        while (line != null){
+            String[] split = line.split(";");
+            list.add(new Object[]{new ContactData().withFirstname(split[0]).withLastname(split[1])
+            .withNickname(split[2]).withTitle(split[3]).withHome(split[4])});
+            line = reader.readLine();
+        }
+        return list.iterator();
+
+    }
+
+
+    @Test(dataProvider = "validContacts")
+    public void initContactCreation(ContactData contact) {
         Contacts before = app.contact().all();
         app.goTo().homePage();
-        File photo = new File("src\\test\\java\\sandbox\\addressbook\\test\\resourses\\foto.jpg");
-        ContactData contact = new ContactData().withFirstname("James").withLastname("Jones")
-                .withNickname("Jam.jones").withTitle("QA").withCompany("Infotecs")
-                .withAddress("Manchester, Stadium Old Trafford").withHome("2780857")
-                .withMobile("89053555178").withEmail("james.jones@oldtrafford.com")
-                .withBday("17").withByear("1985").withPhoto(photo);
         app.contact().addContact(contact);
         Set<ContactData> after = app.contact().all();
         assertThat(after.size(), equalTo(before.size() + 1));
-        assertThat(after,
-                equalTo(before.withAdded(contact.withId(after.stream().mapToInt((c) ->
-                        c.getId()).max().getAsInt()))));
+
     }
 
 }
