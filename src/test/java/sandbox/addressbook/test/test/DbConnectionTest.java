6e145d19372b7937bbc1b7e143beb32a0b079d3e
artur <fazlyev.art@gmail.com>
2019-09-26 16:32:59 +0500

    Реализовано подключение к БД

---
+++ src/test/java/sandbox/addressbook/test/test/DbConnectionTest.java
@@ -0,0 +1,42 @@
+package sandbox.addressbook.test.test;
+
+import org.testng.annotations.Test;
+import sandbox.addressbook.test.modele.GroupData;
+import sandbox.addressbook.test.modele.Groups;
+
+import java.sql.*;
+
+public class DbConnectionTest {
+    @Test
+    public void testDbConnection() {
+
+        Connection conn = null;
+        try {
+            conn =
+                    DriverManager.getConnection("jdbc:mysql://localhost:3306" +
+                            "/addressbook?serverTimezone=UTC&user=root&password=");
+            Statement st =  conn.createStatement();
+            ResultSet rs = st.executeQuery
+                    ("SELECT group_header,group_footer,group_id,group_name FROM group_list ");
+            Groups groups = new Groups();
+            while (rs.next()){
+               groups.add(new GroupData().withId(rs.getInt("group_id"))
+                        .withName(rs.getString("group_name"))
+                        .withHeader(rs.getString("group_header"))
+                        .withFooter(rs.getString("group_footer")));
+            }
+            rs.close();
+            st.close();
+            conn.close();
+            System.out.println(groups);
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+
+        }
+
+    }
+}
