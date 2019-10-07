976c1db12c7591adaaf74c8d118f1a883e13eddd
artur <fazlyev.art@gmail.com>
2019-10-07 21:55:06 +0500

    импорт hibernate

---
+++ src/test/java/addressbook/test/test/HbConnectionTest.java
@@ -0,0 +1,46 @@
+package addressbook.test.test;
+
+import addressbook.test.model.GroupData;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class HbConnectionTest {
+
+    private SessionFactory sessionFactory;
+
+
+    @BeforeMethod
+    protected void setUp() throws Exception {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+        try {
+            sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testHbConnection(){
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<GroupData> result = session.createQuery( "from GroupData" ).list();
+        for ( GroupData group : result ) {
+            System.out.println(group);
+        }
+        session.getTransaction().commit();
+        session.close();
+
+
+
+    }
+}
